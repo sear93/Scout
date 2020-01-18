6c0c359c5bcf598b221b69602db29571ce7ddc9e
Egor <egor.rajewski@gmail.com>
2020-01-18 02:43:36 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ $(document).ready(function () {
         $('.header__burger,.header-nav').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+   $(".news-container").owlCarousel({
+     loop: true,
+     margin: 20,
+     responsiveClass: true,
+     autoplay: true,
+     autoplayTimeout: 10000,
+     responsive: {
+       0: {
+         items: 1,
+         nav: true
+       },
+       500: {
+         items: 2,
+         nav: true
+       },
+       820: {
+         items: 3,
+         nav: true
+       }
+     }
+   });
 });
